927e2edd9864a08ac4a15dd2e947515fcc088104
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-08 12:21:03 -0300

    logic to remove task

--- src/pages/listTask/ListTask.jsx
+++ src/pages/listTask/ListTask.jsx
@@ -1,12 +1,13 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import React, { useContext, useState } from "react";
 import TaskContext  from "../../contexts/TaskContext";
 import DropDown from "../../components/DropDown";
 import { Button, TextArea } from "@radix-ui/themes";
 
 export const ListTask = () => {
-  const { tasks } = useContext(TaskContext);
+  const { tasks, removeTask } = useContext(TaskContext);
   const { id } = useParams();
+  const navigate = useNavigate();
   const taskListed = tasks.find(task => task.id === parseInt(id));
   const [title, setTitle] = useState(taskListed?.title || '');
   const [description, setDescription] = useState(taskListed?.description || '');
@@ -21,14 +22,14 @@ export const ListTask = () => {
       </div>
       <div className="listTaskActions">
         <DropDown />
-        <Button variant="solid" color="red" size='2'>Deletar atividade</Button>
+        <Button variant="solid" color="red" size='2' onClick={() => {removeTask(parseInt(id), navigate, taskListed)}}>Deletar atividade</Button>
         <Button variant="surface" color="gray" size='2'>Editar informações</Button>
       </div>
 
     </div>
     <div className="listTaskDescription">
       <h3>Descrição</h3>
-      <TextArea disabled className="textarea">{description}</TextArea>
+      <TextArea disabled className="textarea" value={description}/>
     </div>
     </>
   );
