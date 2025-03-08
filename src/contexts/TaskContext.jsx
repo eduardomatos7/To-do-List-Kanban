549e5263aabd5462d3274d81526a7f1433d1ae93
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-08 12:17:06 -0300

    feat: adding function to remove task

--- src/contexts/TaskContext.jsx
+++ src/contexts/TaskContext.jsx
@@ -5,21 +5,26 @@ const TaskContext = createContext();
 
 export const TaskProvider = ({ children }) => {
     const [tasks, setTasks] = useState([
-        {/*TESTES
-            new Task(1, 'Tarefa 1', 'Descrição da tarefa 1', 'todo', 'low'), 
+        new Task(1, 'Tarefa 1', 'Descrição da tarefa 1', 'todo', 'low'), 
         new Task(2, 'Tarefa 2', 'Descrição da tarefa 2', 'doing', 'medium'), 
-        new Task(3, 'Tarefa 3', 'Descrição da tarefa 3', 'done', 'high')]);
-    */}]);
+        new Task(3, 'Tarefa 3', 'Descrição da tarefa 3', 'done', 'high')
+    ]);
+
     const addTask = (task) => {
         setTasks([...tasks, task]);
     };
-    /*const removeTask = (id) => {
+    const removeTask = (id, navigate, task) => {
         const newTasks = tasks.filter((task) => task.id !== id);
-        setTasks(newTasks);
-    }*/
+        confirm('Deseja realmente excluir essa tarefa?') 
+            ? 
+            (setTasks(newTasks), (navigate && navigate('/')))
+            :
+            console.log('Task not removed:', id);
+        
+    }
 
     return (
-        <TaskContext.Provider value={{ tasks, addTask }}>
+        <TaskContext.Provider value={{ tasks, addTask, removeTask }}>
             {children}
         </TaskContext.Provider>
     );
