a47ad62378b104344cc3968cbde9d661311b0c35
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-09 18:20:09 -0300

    refactor: code refactoring and reformatting

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EditTask from '../components/EditTask';
 import App from '../App';
-import ErrorBoundary from '../components/ErrorBoundary';
 import {ListTask} from '../pages/listTask/ListTask';
 import {TaskProvider} from '../contexts/TaskContext';
 
 export const AppRouts = () => {
     return (
         <Router>
-            <ErrorBoundary>
                 <TaskProvider>
                     <Routes>
                         <Route path='/' element={<App />} />
@@ -16,7 +14,6 @@ export const AppRouts = () => {
                         <Route path='/listTask/:id' element={<ListTask />} />
                     </Routes>
                 </TaskProvider>
-            </ErrorBoundary>
         </Router>
     )
 }
